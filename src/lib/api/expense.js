7c2f39a11b7061aada0f7a1b0583164ea051ec09
sorakim <sora6418@gmail.com>
2024-06-14 09:36:42 +0900

    Fix: json server url 변경

--- src/lib/api/expense.js
+++ src/lib/api/expense.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const JSON_SERVER_HOST = "http://localhost:5002";
+const JSON_SERVER_HOST = "https://wooden-aware-fowl.glitch.me";
 
 export const getExpenses = async () => {
   try {
